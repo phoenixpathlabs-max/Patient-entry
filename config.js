e004fe62d41a8f7f7d1663b5fda897e5ac826da7
phoenixpathlabs-max <phoenixpathlabs@gmail.com>
2025-09-19 00:30:09 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+// Put your Google Apps Script Web App URL here (example: "https://script.google.com/macros/s/XXXXXXXX/exec")
+const SCRIPT_URL = ""; // <-- REPLACE with your deployed Web App URL (or leave blank to use localStorage only)
